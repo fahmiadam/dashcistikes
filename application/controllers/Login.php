a652a4c0852e31dd02384df858b8a167368dba2b
fahmi adam <fahmiadam66@gmail.com>
2019-12-02 11:53:32 +0700

    update lagi

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -42,6 +42,6 @@ class Login extends CI_Controller {
     public function logout()
     {
         $this->session->sess_destroy();
-        redirect(base_url('login'));
+        redirect(base_url('index.php/login'));
     }
 }
